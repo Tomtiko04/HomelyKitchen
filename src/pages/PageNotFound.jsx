12f276cd8cecfaa8710252a9d1c3d3bd3264166e
oyinkansola <ogunneyeoyinkansola5@gmail.com>
2024-05-20 01:24:42 +0100

    Designed page not found

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,5 +1,10 @@
+import Button from "../UI/Button";
+
 export default function PageNotFound() {
   return (
-    <div>PageNotFound</div>
+    <div className="pageNotFound">
+      <h2>PAGE NOT FOUND</h2>
+      <Button type="primary">Home</Button>
+    </div>
   )
 }
