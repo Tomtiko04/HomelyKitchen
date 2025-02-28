5a3d60b232bfcfce0cceafa4d389916cd28f358d
oyinkansola <ogunneyeoyinkansola5@gmail.com>
2025-02-28 01:22:30 +0100

    Changed empty cart message

--- src/UI/Filter.jsx
+++ src/UI/Filter.jsx
@@ -14,6 +14,7 @@ export default function Filter({filterField, options}) {
 		<select
 			onChange={(e) => handleClick(e.target.value)}
 			value={searchParams.get(filterField) || options[0].value}
+			name={filterField}
 			className="border rounded py-2 w-60 px-3 shadow bg-white outline-none focus:outline-none focus:ring focus:ring-orange-500 ">
 			{options.map((option) => (
 				<option
--- src/feature/cart/EmptyCart.jsx
+++ src/feature/cart/EmptyCart.jsx
@@ -6,10 +6,12 @@ export default function EmptyCart() {
     const navigate = useNavigate()
 	return (
 		<div className="flex flex-col items-center justify-center gap-y-5 mb-12">
-			<img className="block w-36" src="/empty.png"/>
-			<p>Your cart is empty</p>
-			<p>No item is found in your cart.</p>
-            <Button type="primary" onClick={()=> navigate("/menus/all")}>Check menu</Button>
+			<img className="block w-36" src="/empty.png" />
+			<p>Oops, your cart is currently empty!</p>
+			<p>No items found in your cart. Tap the button below to explore our menu!</p>
+			<Button type="primary" onClick={() => navigate("/menus/all")}>
+				Check menu
+			</Button>
 		</div>
 	);
 }
